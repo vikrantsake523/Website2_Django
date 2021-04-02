0fd772979f69b6d7ff42f729556c2b697220e090
vikrantsake523 <vikrantsake523@gmail.com>
2021-04-02 11:07:46 +0530

    changes

--- ECWebsite/Shop/urls.py
+++ ECWebsite/Shop/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index,name = "HomePage"),
+    path('', views.index, name = "ShopHomePage"),
+    path('about/', views.about, name = "AboutUs"),
+    path('contact/', views.contact, name = "ContactUs"),
+    path('tracker/', views.tracker, name = "TrackUs"),
+    path('search/', views.search, name = "Search"),
+    path('productview/', views.productview, name = "Productview"),
+    path('checkout/', views.checkout, name = "checkout")
+
 ]
--- ECWebsite/Shop/views.py
+++ ECWebsite/Shop/views.py
@@ -3,5 +3,22 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    # return HttpResponse("Shop HomePage")
     return render(request,"shop/index.html")
+
+def about(request):
+    return HttpResponse("we are in about")
+
+def contact(request):
+    return HttpResponse("we are in contact")
+
+def tracker(request):
+    return HttpResponse("we are in about")
+
+def search(request):
+    return HttpResponse("we are in about")
+
+def productview(request):
+    return HttpResponse("we are in about")
+
+def checkout(request):
+    return HttpResponse("we are in about")
